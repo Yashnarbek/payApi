a030306c1dcea8dc5bc3bf3a0ef4a6066db79e67
Yashnarbek <ymqayumov@gmail.com>
2022-08-25 12:51:10 +0300

    Correct mistakes

--- js/main.js
+++ js/main.js
@@ -1,12 +1,15 @@
 let elBurger = document.querySelector(".site-header__burger")
 let elNav = document.querySelector(".sitenav")
 let elClose = document.querySelector(".sitenav__close")
+let elbody = document.querySelector("body")
 
 elBurger.addEventListener("click", function(){
     elNav.classList.add("open")
+    elbody.classList.add("overflow")
 })
 
 elClose.addEventListener("click", function(){
     elNav.classList.remove("open")
     elNav.classList.remove("close")
+    elbody.classList.remove("overflow")
 })
